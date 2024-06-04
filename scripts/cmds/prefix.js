7f4163b262226742817f897cccadfd47d66d8733
MOHAMMAD RAJ <mohammadrajvai3@gmail.com>
2024-06-04 12:28:56 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -5,7 +5,7 @@ module.exports = {
  config: {
    name: "prefix",
    version: "1.0",
-   author: "Ahmed Sojib",
+   author: "Mohammad raj",
    countDown: 5,
    role: 0,
    shortDescription: "no prefix",
@@ -17,7 +17,7 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
- body: `✨𝗛𝗶 𝗙𝗿𝗶𝗲𝗻𝗱!✨\n\n✨𝗧𝗵𝗶𝘀 𝗶𝘀 𝗺𝘆 𝗣𝗿𝗲𝗳𝗶𝘅 [ / ]\n
+ body: `✨𝗛𝗶 𝗙𝗿𝗶𝗲𝗻𝗱!✨\n\n✨𝗧𝗵𝗶𝘀 𝗶𝘀 𝗺𝘆 𝗣𝗿𝗲𝗳𝗶𝘅 [ ! ]\n
 𝗧𝗢 𝗩𝗜𝗘𝗪 𝗔𝗟𝗟 𝗢𝗙 𝗧𝗛𝗘\n𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘  𝗖𝗢𝗠𝗠𝗔𝗡𝗗, 𝗧𝗬𝗣𝗘 [/𝗛𝗘𝗟𝗣]`,
   attachment: await global.utils.getStreamFromURL("")
  });
